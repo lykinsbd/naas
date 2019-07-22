67019baf54a676e1596c8fd992d5f8fbac69cbda
Brett Lykins <brett.lykins@rackspace.com>
2019-07-22 16:40:26 -0500

    black @120 lines w 19.3b0

--- setup.py
+++ setup.py
@@ -9,9 +9,4 @@ from naas import __version__
 #   version: necessary for pip, etc. to tell whether this package meets its requirements. For example, if you want
 #        to install "naas>=2.3", is this package even worth downloading?
 
-setup(
-    name="naas",
-    setup_requires=["setuptools>=30.3"],
-    test_suite="naas.tests",
-    version=__version__,
-)
+setup(name="naas", setup_requires=["setuptools>=30.3"], test_suite="naas.tests", version=__version__)
