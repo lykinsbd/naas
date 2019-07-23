b05eff1c8d95854a4d9356c7cc0a37fa8f3f5746
Brett Lykins <brett.lykins@rackspace.com>
2019-07-23 14:39:29 -0500

    refactor logging to be more streamlined

--- gunicorn.py
+++ gunicorn.py
@@ -2,6 +2,7 @@ from naas.library.selfsigned import generate_selfsigned_cert
 from os import environ
 
 
+# Setup basic attributes of our web-server
 bind = "0.0.0.0:5000"
 chdir = "/app"
 
@@ -9,8 +10,35 @@ workers = 50
 worker_class = "sync"
 timeout = 300
 
-accesslog = "-"
-access_log_format = '%(t)s %(p)s %(h)s %({X-Forwarded-For}i)s %(l)s "%(r)s" %(s)s %(b)s "%(f)s"'
+# Configure access logging
+accesslog = "-"  # "-" means log to stdout
+# Format: date proc_name remote_addr X-Forwarded-For X-Request-ID method/path status_code response_length user_agent
+access_log_format = '%(t)s %(p)s %(h)s %({X-Forwarded-For}i)s %({X-Request-ID}i)s "%(r)s" %(s)s %(b)s "%(a)s"'
+
+# Gather the app environment and assign logging_level based on that
+if "dev" in environ.get("APP_ENVIRONMENT", "dev"):
+    logging_level = environ.get("LOG_LEVEL", "DEBUG")
+else:
+    logging_level = environ.get("LOG_LEVEL", "INFO")
+
+# Push our log level up to an environment variable.
+environ["LOG_LEVEL"] = logging_level
+
+# Configure all other (error) logging
+logconfig_dict = {
+    "version": 1,
+    "formatters": {
+        "stderr_format": {
+            "format": "[%(asctime)s] [%(process)d] [%(levelname)s] %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S %z",
+        }
+    },
+    "handlers": {
+        "NAAS_handler": {"class": "logging.StreamHandler", "level": logging_level, "formatter": "stderr_format"}
+    },
+    "loggers": {"NAAS": {"level": logging_level, "handlers": ["NAAS_handler"]}},
+    "disable_existing_loggers": False,
+}
 
 # Try and find TLS file names
 KEY_FILE = environ.get("KEY_FILE", None)
