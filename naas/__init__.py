a1c05aeac859623f9f649ac108d9731cbab55ca7
Brett Lykins <lykinsbd@gmail.com>
2019-08-24 12:52:18 -0500

    fixes #16 and refactors validation logic
    In addition to fixing #16, this also largely refactors the validation
     class, and makes the  `valid_post()` decorator more useful.
    
    Reduced code duplication in `send_command` and `send_config` resources.

--- naas/__init__.py
+++ naas/__init__.py
@@ -6,3 +6,4 @@ Initialization module for NAAS. Sets version
 """
 
 __version__ = "0.5.1"
+__base_response__ = {"app": "naas", "version": __version__}
--- naas/library/decorators.py
+++ naas/library/decorators.py
@@ -4,40 +4,59 @@
 from flask import current_app, g, request
 from functools import wraps
 from naas.library import validation
+from naas.library.auth import Credentials
 from uuid import uuid4
 
 
 def valid_post(f):
     """
-    Decorator function to check validity of a NAAS payload
+    Decorator function to check validity of a POSTed NAAS payload
     :param f:
     :return:
     """
 
     @wraps(f)
     def wrapper(*args, **kwargs):
+        """
+        Perform validation and other actions on this request and payload
+        :param args:
+        :param kwargs:
+        :return:
+        """
+
         v = validation.Validate()
         v.has_auth()
+        v.locked_out()
         v.is_json()
-        v.is_ip_addr(request.json["ip"], "ip")
+        v.is_ip_addr()
         v.is_command_set()
-        v.custom_port()
+        v.has_port()
         v.save_config()
         v.commit()
         v.has_device_type()
 
-        # Capture or create the x-request-id
-        if "x-request-id" not in v.http.headers.keys():
+        # Capture or create the x-request-id, and store it on the g object
+        if "x-request-id" not in v.headers.keys():
             g.request_id = str(uuid4())
         else:
-            v.is_uuid(uuid=v.http.headers["x-request-id"])
-            g.request_id = v.http.headers["x-request-id"]
+            v.is_uuid(uuid=v.headers["x-request-id"])
+            g.request_id = v.headers["x-request-id"]
+
+        # Validate if there's a job ID by this x-request-id already:
+        v.is_duplicate_job(g.request_id)
+
+        # Create a credentials object, and store it on the g object
+        g.credentials = Credentials(
+            username=request.authorization.username,
+            password=request.authorization.password,
+            enable=request.json.get("enable", None),
+        )
 
         # Log this request's details
         current_app.logger.info(
             "%s: %s is issuing %s command(s) to %s:%s",
             g.request_id,
-            request.authorization.username,
+            g.credentials.username,
             len(request.json["commands"]),
             request.json["ip"],
             request.json["port"],
@@ -45,7 +64,7 @@ def valid_post(f):
         current_app.logger.debug(
             "%s: %s is issuing the following commands to %s:%s: %s",
             g.request_id,
-            request.authorization.username,
+            g.credentials.username,
             request.json["ip"],
             request.json["port"],
             request.json["commands"],
